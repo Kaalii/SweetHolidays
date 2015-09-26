96e21693c22cb9d9e872dfd8560553ed362668b8
Blanco68 <blanco_number_1@hotmail.fr>
2015-09-26 09:56:55 +0200

    Ajout d'un commentaire

--- SweetHolidays/src/main/java/fr/uha/miage/sweetholidays/SweetHolidaysApplication.java
+++ SweetHolidays/src/main/java/fr/uha/miage/sweetholidays/SweetHolidaysApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SweetHolidaysApplication
 {
-	
+	//Un commentaire
 	public static void main(String[] args)
 	{
 		SpringApplication.run(SweetHolidaysApplication.class, args);
