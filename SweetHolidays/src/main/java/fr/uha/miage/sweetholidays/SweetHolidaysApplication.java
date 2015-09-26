a77a7cb72a1d7b7c240019e52d5867b47b959e8d
Mollusk68 <olivier.muller1@uha.fr>
2015-09-26 09:27:33 +0200

    First commit

---
+++ SweetHolidays/src/main/java/fr/uha/miage/sweetholidays/SweetHolidaysApplication.java
@@ -0,0 +1,14 @@
+package fr.uha.miage.sweetholidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHolidaysApplication
+{
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SweetHolidaysApplication.class, args);
+	}
+}
---
+++ SweetHolidays/src/test/java/fr/uha/miage/sweetholidays/SweetHolidaysApplicationTests.java
@@ -0,0 +1,18 @@
+package fr.uha.miage.sweetholidays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SweetHolidaysApplication.class)
+@WebAppConfiguration
+public class SweetHolidaysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
