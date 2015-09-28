480fa7b4ccd2a0a6b74ed2821802568f735e13b8
e1201224 <e1201224@FST-I39-PC25.uha.fr>
2015-09-28 11:45:23 +0200

    Ajout du web controller

---
+++ SweetHolidays/src/main/java/fr/uha/miage/sweetholidays/controler/WebControler.java
@@ -0,0 +1,58 @@
+package fr.uha.miage.sweetholidays.controler;
+
+
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestMethod;
+	import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+	import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+	@Controller
+	public class WebControler extends WebMvcConfigurerAdapter {
+
+	    @Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/results").setViewName("results");
+	    }
+
+	    @RequestMapping(value="/SweetHolidays", method=RequestMethod.GET)
+	    public String showIndex() {
+	        return "index";
+	    }
+	    
+	    @RequestMapping(value="/SweetPlace", method=RequestMethod.GET)
+	    public String showPlaces() {
+	        return "rooms";
+	    }
+	    
+	    @RequestMapping(value="/SweetResa", method=RequestMethod.GET)
+	    public String showResa() {
+	        return "reservation";
+	    }
+	    
+	    @RequestMapping(value="/SweetActivities", method=RequestMethod.GET)
+	    public String showActi() {
+	        return "activities";
+	    }
+	    
+	    @RequestMapping(value="/SweetContact", method=RequestMethod.GET)
+	    public String showContact() {
+	        return "contact";
+	    }
+	    
+	    @RequestMapping(value="/SweetDetails", method=RequestMethod.GET)
+	    public String showDetail() {
+	        return "details";
+	    }
+
+
+	    /*@RequestMapping(value="/", method=RequestMethod.POST)
+	    public String checkPersonInfo(BindingResult bindingResult) {
+	        if (bindingResult.hasErrors()) {
+	            return "form";
+	        }
+	        return "redirect:/results";
+	    }*/
+
+	}
